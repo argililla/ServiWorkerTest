42a3838b7a5a35e0bf260e0ae3c371b88a5630ef
argililla <argililla@gmail.com>
2018-09-20 14:13:54 +0200

    bugfixes

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,3 @@
-// 'use strict'
-
-// var cacheVersion = 1;
-// var currentCache = {
-//     offline: 'offline-cache' + cacheVersion
-// };
-
-// const offlineUrl = 'offline-page.html';
-
-// this.addEventListener('install', event => {
-//     event.waitUntil(
-//       caches.open(currentCache.offline).then(function(cache) {
-//         return cache.addAll([
-//             './img/wario.png',
-//             offlineUrl
-//         ]);
-//       })
-//     );
-// });
-
-// this.addEventListener('fetch', event => {
-//     // request.mode = navigate isn't supported in all browsers
-//     // so include a check for Accept: text/html header.
-//     if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-//           event.respondWith(
-//             fetch(event.request.url).catch(error => {
-//                 // Return the offline page
-//                 return caches.match(offlineUrl);
-//             })
-//       );
-//     }
-//     else{
-//           // Respond with everything else if we can
-//           event.respondWith(caches.match(event.request)
-//                           .then(function (response) {
-//                           return response || fetch(event.request);
-//                       })
-//               );
-//         }
-//   });
-
 var CACHE_NAME = 'offline-page';
 var urlsToCache = [
   '/',
@@ -92,3 +51,44 @@ self.addEventListener('fetch', function(event) {
       )
     );
   });
+
+  // 'use strict'
+
+// var cacheVersion = 1;
+// var currentCache = {
+//     offline: 'offline-cache' + cacheVersion
+// };
+
+// const offlineUrl = 'offline-page.html';
+
+// this.addEventListener('install', event => {
+//     event.waitUntil(
+//       caches.open(currentCache.offline).then(function(cache) {
+//         return cache.addAll([
+//             './img/wario.png',
+//             offlineUrl
+//         ]);
+//       })
+//     );
+// });
+
+// this.addEventListener('fetch', event => {
+//     // request.mode = navigate isn't supported in all browsers
+//     // so include a check for Accept: text/html header.
+//     if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+//           event.respondWith(
+//             fetch(event.request.url).catch(error => {
+//                 // Return the offline page
+//                 return caches.match(offlineUrl);
+//             })
+//       );
+//     }
+//     else{
+//           // Respond with everything else if we can
+//           event.respondWith(caches.match(event.request)
+//                           .then(function (response) {
+//                           return response || fetch(event.request);
+//                       })
+//               );
+//         }
+//   });
