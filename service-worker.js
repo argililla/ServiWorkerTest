d1760cfe6770a404d3166404b0e9ed5267d7d7d4
argililla <argililla@gmail.com>
2018-09-24 14:51:05 +0200

    added online status snackbar

--- service-worker.js
+++ service-worker.js
@@ -28,48 +28,102 @@ var resourcesToCache = [
   './files/DocumentoPrueba10.pdf'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   // install files needed offline
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(function(cache) {
+      .then(function (cache) {
         console.log('Opened cache');
+
         return cache.addAll(resourcesToCache);
       })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-    // every request from our site, passes through the fetch handler
-    // I have proof
-    console.log('I am a request with url:', event.request.clone().url)
-    event.respondWith(
-      // check all the caches in the browser and find
-      // out whether our request is in any of them
-      caches.match(event.request)
-        .then(function(response) {
-          if (response) {
-            console.log('Found ', event.request.url, ' in cache');
-            // if we are here, that means there's a match
-            //return the response stored in browser
-            return response;
+self.addEventListener('fetch', function (event) {
+  // every request from our site, passes through the fetch handler
+  // I have proof
+  console.log('I am a request with url:', event.request.clone().url)
+  event.respondWith(
+    // check all the caches in the browser and find
+    // out whether our request is in any of them
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          // if we are here, that means there's a match
+          //return the response stored in browser
+          return response;
         }
-          // no match in cache, use the network instead
-          console.log('Network request for ', event.request.url);
-          return fetch(event.request).then(function(response){
-              if(response.status === 404) {
-                  console.log("page not founf: Error 404");
-              }
-            return caches.open(resourcesToCache).then(function(cache) {
-                cache.put(event.request.url, response.clone());
-                return response;
-            });
+        // no match in cache, use the network instead
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then(function (response) {
+          if (response.status === 404) {
+            console.log("page not founf: Error 404");
+          }
+          return caches.open(resourcesToCache).then(function (cache) {
+            cache.put(event.request.url, response.clone());
+            return response;
           });
-        }).catch (function(error) {
-            console.log('Error', error);
-        })
-    );
-  });
+        });
+      }).catch(function (error) {
+        console.log('Error', error);
+      })
+  );
+});
+
+self.addEventListener('online', function (e) {
+  console.log('EventListener: ONline');
+  // if(this.navigator.onLine) { 
+  //   console.log('EventListener: ONline');
+  //   //self.showSnackbarOnline();
+  // }
+  var x = document.getElementById("snackbarOnline");
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+});
+
+self.addEventListener('offline', function (e) {
+  console.log('EventListener: OFFline');
+  // if(!this.navigator.onLine) {
+  //   console.log('EventListener: OFFline');
+  //   //this.showSnackbarOffline();
+  // }
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbarOffline");
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+});
+
+// function showSnackbarOnline() {
+//   console.log('Mostrando Snaackbar ONline');
+//   // Get the snackbar DIV
+//   var x = document.getElementById("snackbarOnline");
+
+//   // Add the "show" class to DIV
+//   x.className = "show";
+
+//   // After 3 seconds, remove the show class from DIV
+//   setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+// }
+
+// function showSnackbarOffline() {
+//   console.log('Mostrando Snaackbar OFFline');
+//   // Get the snackbar DIV
+//   var x = document.getElementById("snackbarOffline");
+//   // Add the "show" class to DIV
+//   x.className = "show";
+
+//   // After 3 seconds, remove the show class from DIV
+//   setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+// }
 
   // 'use strict'
 
